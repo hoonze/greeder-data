492881c28499609877146d1b864293982b115307
Jihoon Byun <wlgns1588@naver.com>
2021-09-17 17:52:28 +0900

    chore: 쿼리로 키워드별 리스트 반환

--- imageCrawler/views.py
+++ imageCrawler/views.py
@@ -6,7 +6,15 @@ from rest_framework import viewsets
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
-    serializer_class =ImageSerializer
+    serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        keyword = self.request.query_params.get('keyword', '')
+        if keyword:
+            qs = qs.filter(keyword=keyword)
+        return qs
 
     # def get(self, request):
     #     # 여러개의 객체를 serialization하기 위해 many = True로 설정
