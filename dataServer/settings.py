0a4878088c0bfc51840a26b54c2ef1d9c44829d7
변지훈 <wlgns1588@naver.com>
2021-09-28 18:02:06 +0900

    cors 문제 해결중2

--- dataServer/settings.py
+++ dataServer/settings.py
@@ -43,12 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'crawler',
-    'keywords',
-    'corsheaders'
+    'keywords'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,8 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware'
+
 ]
 
 ROOT_URLCONF = 'dataServer.urls'
